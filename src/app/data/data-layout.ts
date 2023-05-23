3c1f9955c175a5913332b51ebf0d6b6674aaef49
@yohasa <yhsaghrounu@gmail.com>
2023-05-23 15:56:50 +0300

    #

---
+++ src/app/data/data-layout.ts
@@ -0,0 +1,9 @@
+export  class header {
+    title!: string;}
+export const pageHeader:header={
+    title:'cars catalog',
+}
+export  class footer {
+    title!: string;}
+export const pageFooter:footer={
+    title:'cars catalog',}
